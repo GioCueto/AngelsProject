cbe8a2e883c793bd6c66b532b336e9983c48880e
giovanagarcia <giovana.garcu@gmail.com>
2024-05-14 22:03:27 -0600

    todo se esta derrumbando

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+
+  get "/servicios", to: "pages#servicios", as: "servicios"
+  get "/experiencia", to: "pages#experiencia", as: "experiencia"
+  get "/contacto", to: "pages#contacto", as: "contacto"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
