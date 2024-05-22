4dc162ed5b5c0081de663eeac5769224db10bca6
giovanagarcia <giovana.garcu@gmail.com>
2024-05-22 17:40:36 -0600

    Avance en todas las pags, esperando info para terminar experiencia y servicios, voy a avanzar con el blog mientras

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "/servicios", to: "pages#servicios", as: "servicios"
   get "/experiencia", to: "pages#experiencia", as: "experiencia"
   get "/contacto", to: "pages#contacto", as: "contacto"
+  get "/blog", to: "pages#blog", as: "blog"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
